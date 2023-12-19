93972703cc4c06d8f705c64ba09a6e76c5b3e8c2
cfff226 <cazziednb@outlook.com>
2023-12-19 16:15:41 +0000

    Add list which will include the price of each menu item

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -3,7 +3,7 @@ import math
 # A program which allows the user to add, remove and checkout with items that they purchase
 # This program is a simplified version to pet-store and will include functions
 
-# Dictionary of items
+# List of items
 
 menu = [
     "Purina One Cat Salmon - Whole Grain               ",
@@ -15,3 +15,5 @@ menu = [
     "Bakers Whirlers Double Flavour Twisted Treats     ",
     "Chicken And Country Vegetable Dry Dog Food        ",
 ]
+
+prices = [7.99, 5.99, 3.99, 7.00, 4.00, 3.99, 4.00, 6.99]
