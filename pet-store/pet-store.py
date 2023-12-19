3487fbf891e6e9edf5f1cb5afb8b1b3f7b901f74
cfff226 <cazziednb@outlook.com>
2023-12-19 16:14:53 +0000

    Add list which includes each menu item that the user can choose to purchase

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -2,3 +2,16 @@ import math
 
 # A program which allows the user to add, remove and checkout with items that they purchase
 # This program is a simplified version to pet-store and will include functions
+
+# Dictionary of items
+
+menu = [
+    "Purina One Cat Salmon - Whole Grain               ",
+    "Iams Senior 7+ Cat Food With Ocean Fish           ",
+    "Iams Adult 1+ Cat Food With Fresh Chicken         ",
+    "Felix As Good As It Looks Cat Food Mixed Selection",
+    "Felix Doubley Delicious Cat Food Meaty Selection  ",
+    "Winalot Wet Dog Food Pouches Meaty Chunks In Jelly",
+    "Bakers Whirlers Double Flavour Twisted Treats     ",
+    "Chicken And Country Vegetable Dry Dog Food        ",
+]
