8df5869caa99af8608c946017af660280cc33eb2
cfff226 <cazziednb@outlook.com>
2023-12-19 16:12:28 +0000

    Initial commit

---
+++ pet-store/pet-store.py
@@ -0,0 +1,4 @@
+import math
+
+# A program which allows the user to add, remove and checkout with items that they purchase
+# This program is a simplified version to pet-store and will include functions
