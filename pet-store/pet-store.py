a7e715f9a55528bd45272fbd24992ab0b3cda143
cfff226 <cazziednb@outlook.com>
2023-12-20 11:16:40 +0000

    Create class method to add item and declare self, item and price as attributes to pass in - If item is already in the self.items dictionary, then add the price of the new to the dictionary value/cart item total

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -19,7 +19,6 @@ menu = [
 prices = [7.99, 5.99, 3.99, 7.00, 4.00, 3.99, 4.00, 6.99]
 
 # Display the menu and the cart until the user checks out
-
 print("\nWelcome to Pawesome Warehouse \n\nWhat would you like to do?\n")
 
 done = False
@@ -33,8 +32,13 @@ while not done:
     break
     done = True
 
-class ShoppingCart: 
+
+# Class to include all data and functions of the shopping cart
+class ShoppingCart:
     def __init__(self):
         self.items = {}
 
-        
+    def add_item(self, item, price):
+            if item in self.items: 
+                self.items[item] += price 
+            
