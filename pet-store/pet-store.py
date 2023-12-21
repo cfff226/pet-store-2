533df534c2042563671e2dd86132f9789fe5b2ba
cfff226 <cazziednb@outlook.com>
2023-12-21 12:37:09 +0000

    Add the shopping quant argument into the add item function, in all occurances

--- pet-store/pet-store.py
+++ pet-store/pet-store.py
@@ -20,7 +20,7 @@ prices = [7.99, 5.99, 3.99, 7.00, 4.00, 3.99, 4.00, 6.99]
 
 
 # Add item to cart
-def add_item(shopping_cart):
+def add_item(shopping_cart, shopping_quant):
     for i in range(len(menu)):
         print(str(i + 1) + ". " + menu[i], prices[i])
 
@@ -32,7 +32,7 @@ def add_item(shopping_cart):
         )
     except ValueError:
         print("You have entered an incorrect value")
-        add_item(shopping_cart)
+        add_item(shopping_cart, shopping_quant)
 
     while True:
         try:
@@ -69,7 +69,7 @@ def main():
             "\nPlease enter the number of the option that you would like to choose: "
         )
         if choice == "1":
-            add_item(shopping_cart)
+            add_item(shopping_cart, shopping_quant)
         # elif choice == "2":
         #     view_cart(shopping_cart)
         # elif choice == "3":
